3aaee81f26b223d80841a89c64158c38bde18240
shinich39 <shinich39@gmail.com>
2024-08-09 06:58:14 +0900

    fix

--- js/libs/menu.js
+++ js/libs/menu.js
@@ -98,7 +98,7 @@ async function sendToLoadImageNode() {
       const obj = parseURL(url);
       const filePath = parseObjectURL(obj).filePath;
       const node = getLoadImageNode();
-      await node.pkg39.loadImageByFilePath(filePath);
+      await node.pkg39.loadImageByPath(filePath);
     }
   }
 }
